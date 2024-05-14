0bdff07543844dab241e28bca51d2daac60909a0
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-05-14 18:19:08 +0700

    class mahasiswa private dan public

--- Constructor_0050.cpp
+++ Constructor_0050.cpp
@@ -5,25 +5,45 @@ using namespace std;
 class mahasiswa
 {
 public:
-	static int nim;
-	int id;
-	string nama;
+    static  int nim;
+    int id;
+    string nama;
 
-	void setID();
+    void setID();
 
-	void printAll();
-	mahasiswa(string pnama)
-	{
-		nama = pnama;
-		setID();
-	}
+    void printAll();
+    mahasiswa(string pnama)
+    {
+        nama = pnama;
+        setID();
+    }
 };
 
 int mahasiswa::nim = 113;
 
+void mahasiswa::setID()
+{
+    id = nim++;
+}
+
 void mahasiswa::printAll()
 {
-	cout << "ID  = " << id << endl;
-	cout << "Nama = " << nama << endl;
-	cout << endl;
+    cout << "ID    = " << id << endl;
+    cout << "Nama  = " << nama << endl;
+    cout << endl;
+}
+
+int main()
+{
+    mahasiswa mhs1("Lia Kurnia");
+    mahasiswa mhs2("Asroni");
+    mahasiswa mhs3("Andi Kurniawan");
+    mahasiswa mhs4("Joko Purbo");
+
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+
+    return 0;
 }
